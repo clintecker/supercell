50df244959ab737f8f4e58d39372d17702357e8f
Clint Ecker <me@clintecker.com>
2020-09-08 17:53:29 -0600

    v1.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = "2020 Clint Ecker"
 author = "Clint Ecker"
 
 # The short X.Y version
-version = "1.2.0"
+version = "1.2.1"
 # The full version, including alpha/beta/rc tags
-release = "1.2.0"
+release = "1.2.1"
 
 
 # -- General configuration ---------------------------------------------------
